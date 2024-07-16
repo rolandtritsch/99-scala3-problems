a22d8b7140b18007dc3731f81bb53598aabd719d
Roland Tritsch <roland@tritsch.email>
2024-07-16 20:53:24 +0100

    P02: Initial commit.

---
+++ src/P02.scala
@@ -0,0 +1,20 @@
+package ninetynine
+
+/**
+ * P02 - return the last but one element of the list.
+ */
+
+object P02 {
+  final val logger = com.typesafe.scalalogging.Logger(getClass.getName)
+
+  final def penultimate[T](l: List[T]): T = {
+    assert(l.size >= 2, "l.size >= 2")
+    logger.debug(s"${l}")
+
+    l match {
+      case e :: _ :: Nil  => e
+      case _ :: rest => penultimate(rest)
+      case _         => throw new RuntimeException("Unexpected case")
+    }
+  }
+}
--- test/src/P00Test.scala
+++ test/src/P00Test.scala
@@ -8,4 +8,10 @@ class P00Test extends munit.ScalaCheckSuite {
       l.nonEmpty ==> (P01.last(l) == l.last)
     }
   }
+
+  property("P02") {
+    forAll { (l: List[Int]) =>
+      (l.size >= 2) ==> (P02.penultimate(l) == l.take(l.size - 1).last)
+    }
+  }
 }
