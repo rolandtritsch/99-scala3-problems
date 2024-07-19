ec255f409589fd94a3f1e375a175d049e3bf6538
Roland Tritsch <roland@tritsch.email>
2024-07-19 13:48:06 +0100

    P05: Initial commit.

---
+++ src/ninetynine/P05.scala
@@ -0,0 +1,19 @@
+package ninetynine
+
+/** P05 - reverse a list.
+  */
+
+object P05 {
+  final val logger = com.typesafe.scalalogging.Logger(getClass.getName)
+
+  /** @return the reverse of the list l */
+  final def reverse(l: List[Any]): List[Any] = {
+    logger.debug("${l}")
+
+    l match {
+      case Nil  => List()
+      case e :: Nil  => List(e)
+      case e :: rest => reverse(rest) ++ List(e)
+    }
+  }
+}
--- test/src/ninetynine/P00Test.scala
+++ test/src/ninetynine/P00Test.scala
@@ -3,27 +3,33 @@ package ninetynine
 import org.scalacheck.Prop._
 
 class P00Test extends munit.ScalaCheckSuite {
-  property("P01") {
+  property("P01 - last") {
     forAll { (l: List[Int]) =>
       l.nonEmpty ==> (P01.last(l) == l.last)
     }
   }
 
-  property("P02") {
+  property("P02 - penultimate") {
     forAll { (l: List[Int]) =>
       (l.size >= 2) ==> (P02.penultimate(l) == l.take(l.size - 1).last)
     }
   }
 
-  property("P03") {
+  property("P03 - nth") {
     forAll { (n: Int, l: List[Int]) =>
       (n >= 0 && l.size - 1 >= n) ==> (P03.nth(n, l) == l(n))
     }
   }
 
-  property("P04") {
+  property("P04 - size") {
     forAll { (l: List[Int]) =>
       P04.size(l) == l.size
+    }
+  }
+
+  property("P05 - reverse") {
+    forAll { (l: List[Int]) =>
+      P05.reverse(l) == l.reverse
     }
   }
 }
