662575043a40016d603f0b97c253bad2c34fc142
Roland Tritsch <roland@tritsch.email>
2024-07-20 10:03:26 +0100

    P10: Initial commit.

---
+++ src/ninetynine/P10.scala
@@ -0,0 +1,25 @@
+package ninetynine
+
+/** P10 - run-length encoding of a list
+  */
+
+object P10 {
+  final val logger = com.typesafe.scalalogging.Logger(getClass.getName)
+
+  /** @return run-length encoded list */
+  final def encode(l: List[Any]): List[(Int, Any)] = {
+    assert(!l.isEmpty, "!l.empty")
+    logger.debug(s"${l}")
+
+    def encode(l: List[Any], el: List[(Int, Any)], ec: (Int, Any)): List[(Int, Any)] = {
+      logger.debug(s"${l}")
+      l match {
+        case Nil => el ++ List(ec)
+        case e :: rest if e == ec._2 => encode(rest, el, (ec._1 + 1, ec._2))
+        case e :: rest => encode(rest, el ++ List(ec), (1, e))
+      }
+    }
+
+    encode(l.tail, List(), (1, l.head))
+  }
+}
