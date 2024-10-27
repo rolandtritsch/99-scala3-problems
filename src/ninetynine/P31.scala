ed2a956a76b23df5fa6e7d2e4127e105b0bc6dea
Roland Tritsch <roland@tritsch.email>
2024-10-27 11:01:01 +0000

    P31: Revisit. Simplify. Beautify. Refactor AI solution.

--- src/ninetynine/P31.scala
+++ src/ninetynine/P31.scala
@@ -2,7 +2,6 @@ package ninetynine
 
 /** P31 - Determine whether a given integer number is prime.
   *
-  * @note This implementation will be used by [[ninetynine.arithmetic.S99Int]].
   * @note This implements the [[https://www.khanacademy.org/computing/computer-science/cryptography/comp-number-theory/a/trial-division Trial division algorithm]].
   */
 
@@ -14,8 +13,8 @@ object P31 {
     require(n > 1, "n > 1")
     logger.debug(s"${n}")
 
-    val range = 2 to Math.sqrt(n).toInt
-    val isNotPrime = LazyList(range*).exists(n % _ == 0)
+    val wall = 2 to Math.sqrt(n).toInt
+    val isNotPrime = LazyList(wall*).exists(n % _ == 0)
     !isNotPrime
   }
 }
