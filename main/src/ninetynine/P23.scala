0e9ac2ac03ced75a20776f447fe4835be79106ff
Roland Tritsch <roland@tritsch.email>
2025-01-20 15:37:30 +0000

    Run scalafix.

--- main/src/ninetynine/P23.scala
+++ main/src/ninetynine/P23.scala
@@ -1,10 +1,10 @@
 package ninetynine
 
-// scalafix: off
+// scalafix:off
 import scala.util.Random
 
 import com.typesafe.scalalogging.Logger
-// scalafix: on
+// scalafix:on
 
 /** P23 - extract a given number of randomly selected elements from a list.
   */
--- main/src/ninetynine/P25.scala
+++ main/src/ninetynine/P25.scala
@@ -1,10 +1,10 @@
 package ninetynine
 
-// scalafix: off
+// scalafix:off
 import scala.util.Random
 
 import com.typesafe.scalalogging.Logger
-// scalafix: on
+// scalafix:on
 
 /** P25 - generate a random permutation of the elements of a list.
   */
