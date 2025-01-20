d4d131745a5a84fbe231e2990332e057ab4d7bfb
Roland Tritsch <roland@tritsch.email>
2025-01-20 15:32:06 +0000

    Run scalafix.

--- main/test/src/ninetynine/P39Test.scala
+++ main/test/src/ninetynine/P39Test.scala
@@ -5,3 +5,5 @@ class P39Test extends munit.ScalaCheckSuite:
   test("P39 - listPrimesInRange") {
     assertEquals(P39.listPrimesInRange(7 to 31), List(7, 11, 13, 17, 19, 23, 29, 31))
   }
+
+end P39Test
