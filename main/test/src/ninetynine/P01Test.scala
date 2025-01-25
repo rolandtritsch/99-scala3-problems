6b532bc89c2b4428391c6ee3c2cec8f6124e48ea
Roland Tritsch <roland@tritsch.email>
2025-01-25 18:10:03 +0000

    P01: Reformat.

--- main/test/src/ninetynine/P01Test.scala
+++ main/test/src/ninetynine/P01Test.scala
@@ -14,7 +14,18 @@ class P01Test extends munit.ScalaCheckSuite:
 
   property("P01 - last - big list") {
     // 10 string list as input
-    val input = List("first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth", "last")
+    val input = List(
+      "first",
+      "second",
+      "third",
+      "fourth",
+      "fifth",
+      "sixth",
+      "seventh",
+      "eighth",
+      "ninth",
+      "last",
+    )
     val result = P01.last(input)
     val expected = "last"
     assertEquals(result, expected)
