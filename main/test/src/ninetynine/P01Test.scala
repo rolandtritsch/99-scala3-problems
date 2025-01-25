2d6243e91d6820ea79672034f816b5224365f091
Roland Tritsch <roland@tritsch.email>
2025-01-25 18:01:47 +0000

    P01: Add test with long input list.

--- main/test/src/ninetynine/P01Test.scala
+++ main/test/src/ninetynine/P01Test.scala
@@ -12,4 +12,12 @@ class P01Test extends munit.ScalaCheckSuite:
     forAll((l: List[Int]) => (l.nonEmpty) ==> (P01.last(l) == l.last))
   }
 
+  property("P01 - last - big list") {
+    // 10 string list as input
+    val input = List("first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth", "last")
+    val result = P01.last(input)
+    val expected = "last"
+    assertEquals(result, expected)
+  }
+
 end P01Test
