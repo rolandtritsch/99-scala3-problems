46eaf0791ff1585e18fccd44bd834aca2aa85735
Roland Tritsch <roland@tritsch.email>
2025-01-26 12:36:30 +0000

    P56: Reformat.

--- main/test/src/ninetynine/P56Test.scala
+++ main/test/src/ninetynine/P56Test.scala
@@ -1,27 +1,20 @@
 package ninetynine
 
 class P56Test extends munit.ScalaCheckSuite:
+
   import ninetynine.P55.*
   import ninetynine.P56.*
 
   test("P56 - basic"):
-    val symmetricTree = Node('a',
-      Node('b', Node('c'), Node('d')),
-      Node('b', Node('d'), Node('c'))
-    )
-
-    val asymmetricTree = Node('a',
-      Node('b', End, Node('c')),
-      Node('b', End, Node('c'))
-    )
-
+    val symmetricTree = Node('a', Node('b', Node('c'), Node('d')), Node('b', Node('d'), Node('c')))
     assert(symmetricTree.isSymmetric)
+
+    val asymmetricTree = Node('a', Node('b', End, Node('c')), Node('b', End, Node('c')))
     assert(!asymmetricTree.isSymmetric)
 
   test("P56 - no tree with 4 nodes can be symmetric"):
     val result = Tree.cBalanced(4, "x")
     assert(result.forall(!_.isSymmetric))
-
 
   test("P56 - 5 trees with 7 nodes will be symmetric"):
     val result = Tree.cBalanced(7, "x")
