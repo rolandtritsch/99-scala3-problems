64ab7df391bae4026e354fa24127b52c7da6061f
Roland Tritsch <roland@tritsch.email>
2025-01-26 12:34:31 +0000

    P56: Reformat.

--- main/src/ninetynine/P56.scala
+++ main/src/ninetynine/P56.scala
@@ -8,17 +8,20 @@ object P56:
 
   val logger = com.typesafe.scalalogging.Logger(getClass) // scalafix:ok
 
-  extension[T] (tree: Tree[T])
+  extension [T](tree: Tree[T])
+
     def isSymmetric: Boolean =
       def isMirror(t1: Tree[T], t2: Tree[T]): Boolean = (t1, t2) match
         case (End, End) => true
-        case (End, _) => false
-        case (_, End) => false
-        case (Node(v1, l1, r1), Node(v2, l2, r2)) => 
+        case (End, _)   => false
+        case (_, End)   => false
+        // format: off
+        case (Node(v1, l1, r1), Node(v2, l2, r2)) =>
           v1 == v2 && isMirror(l1, r2) && isMirror(r1, l2)
-  
+        // format: on
+
       tree match
         case Node(_, left, right) => isMirror(left, right)
-        case End => true
+        case End                  => true
 
 end P56
