79a8ec5a81428e6d8301673e7ee7bf23ba622e60
Roland Tritsch <roland@tritsch.email>
2025-09-07 06:26:54 +0100

    P06: Add support for ints and lists

--- main/src/ninetynine/P06.scala
+++ main/src/ninetynine/P06.scala
@@ -7,9 +7,12 @@ object P06:
   val logger = com.typesafe.scalalogging.Logger(getClass) // scalafix:ok
 
   /** @return true, if word w is a palindrom */
-  def isPalindrom(w: String): Boolean =
-    logger.debug(s"${w}")
+  def isPalindrom[A](l: List[A]): Boolean =
+    logger.debug(s"l: ${l}")
 
-    w.equals(P05.reverse(w.toList).mkString)
+    P05.reverse(l) == l
+
+  def isPalindrom(w: String): Boolean = isPalindrom(w.toList)
+  def isPalindrom(i: Int): Boolean = isPalindrom(i.toString)
 
 end P06
--- main/test/src/ninetynine/P06Test.scala
+++ main/test/src/ninetynine/P06Test.scala
@@ -5,11 +5,19 @@ import org.scalacheck.Prop.*
 class P06Test extends munit.ScalaCheckSuite:
 
   property("P06 - palindrome"):
+    assert(P06.isPalindrom(""))
+    assert(P06.isPalindrom(" "))
+    assert(!P06.isPalindrom("xyz"))
+
     assert(P06.isPalindrom("abba"))
     assert(P06.isPalindrom("racecar"))
     assert(P06.isPalindrom("madam"))
+
     assert(P06.isPalindrom("())("))
     assert(!P06.isPalindrom("()()"))
+
+    assert(P06.isPalindrom(List(1, 2, 1)))
+    assert(P06.isPalindrom(123321))
 
     forAll: (s: String) =>
       val palindrome = s + s.reverse
